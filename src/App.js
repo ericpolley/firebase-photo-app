499533b46c8c7bff00a42d686d28077dd27e4ad2
eric polley <ericpolley@live.com>
2022-12-20 10:13:49 -0800

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
+import ForgotPassword from './pages/ForgotPassword';
+import Profile from './pages/Profile';
+import Offers from './pages/Offers';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
@@ -8,6 +13,11 @@ function App() {
      <Router>
       <Routes>
         <Route path='/' element={<Home />}></Route>
+        <Route path='/forgotpassword' element={<ForgotPassword />}></Route>
+        <Route path='/profile' element={<Profile />}></Route>
+        <Route path='/offers' element={<Offers />}></Route>
+        <Route path='/signin' element={<SignIn />}></Route>
+        <Route path='/signup' element={<SignUp />}></Route>
       </Routes>
      </Router>
     </div>
