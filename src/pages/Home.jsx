85c731737ab717e7ba8bf5cf4073f6d12146ed38
eric polley <ericpolley@live.com>
2022-12-23 17:33:07 -0800

    homepage text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,12 @@ export default function Home() {
   return (
     <div className='text-center text-lg'>
     <p className='text-3xl font-bold'>WELCOME</p>
-    <br />
-    <p>This is the official night owl website.</p>
+
     <br /><br />
     <p>Sign Up!</p>
-    <p>Sign In!</p>
     <br />
-    <p>Meet a fellow Night Owl.</p>
+    <p>Sign In!</p>
+
     <br /><br />
     <p>That is the idea.</p>
     <br /><br /><br /><br /><br />
